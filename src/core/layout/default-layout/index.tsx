38bdbd45fac45af4f87efee6f0252b635b0917d5
Théo Landemaine <91159174+TheoLandemaine@users.noreply.github.com>
2024-10-22 16:43:07 +0200

    Feat : add bg image

--- src/core/layout/default-layout/index.tsx
+++ src/core/layout/default-layout/index.tsx
@@ -2,7 +2,16 @@ import { PropsWithChildren } from "react";
 
 export const DefaultLayout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="fixed w-screen h-screen top-0 right-0 bottom-0 left-0 overflow-hidden overflow-y-auto scroll-smooth bg-bg text-black thin-scrollbar">
+    <div
+      className="fixed w-screen h-screen top-0 right-0 bottom-0 left-0 overflow-hidden overflow-y-auto scroll-smooth bg-bg text-black thin-scrollbar"
+      style={{
+        // center no-repeat fixed cover
+        backgroundImage: "url('/images/bg.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       {children}
     </div>
   );
