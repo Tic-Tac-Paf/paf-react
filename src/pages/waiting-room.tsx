b180c6ff39505cb955f1f62a9112f4bfd1edcb8f
Théo Landemaine <91159174+TheoLandemaine@users.noreply.github.com>
2024-10-22 16:44:39 +0200

    Feat : update waiting-room for diff display for admin

--- src/pages/waiting-room.tsx
+++ src/pages/waiting-room.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { OutlinedButton } from "../core/ui/buttons";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { SelectInput, SelectOption, TextInput } from "../core/ui/form-inputs";
 import UserIcon from "../assets/img/user-icon";
 import { Player, Room } from "../core/types/room";
@@ -12,14 +12,15 @@ const difficulties: SelectOption[] = [
 ];
 
 export const WaitingRoomScreen: React.FC = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const { id } = useParams(); // Le code de la room
   const playerId = localStorage.getItem("playerId");
 
   const [room, setRoom] = useState<Room | null>(null); // L'état de la room récupéré via WebSocket
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
-    const ws = new WebSocket("ws://paf-api.onrender.com");
+    const ws = new WebSocket("wss://paf-api.onrender.com");
 
     if (ws && id) {
       ws.onopen = () => {
@@ -31,16 +32,17 @@ export const WaitingRoomScreen: React.FC = () => {
 
         if (data.type === "roomInfo" && data.room.code === id) {
           setRoom(data.room);
+          setIsAdmin(data.room.admin.id === playerId);
         } else if (data.type === "updatedRoom" && data.room.code === id) {
           setRoom(data.room);
         }
       };
     }
-  }, [id]);
+  }, [setIsAdmin, id, playerId]);
 
   const handleUpdateRoom = useCallback(
     (key: "rounds" | "difficulty", value: string | number) => {
-      const ws = new WebSocket("ws://paf-api.onrender.com");
+      const ws = new WebSocket("wss://paf-api.onrender.com");
 
       if (typeof value === "number") {
         if (value < 1 || value > 10) {
@@ -66,12 +68,28 @@ export const WaitingRoomScreen: React.FC = () => {
   );
 
   const handleStartRoom = () => {
-    if (room?.players && room?.players?.length < 2) {
-      return alert("Il faut au moins 2 joueurs pour commencer");
-    }
+    // if (room?.players && room?.players?.length < 2) {
+    //   return alert("Il faut au moins 2 joueurs pour commencer");
+    // }
+
+    const ws = new WebSocket("wss://paf-api.onrender.com");
+
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({ type: "getRoomQuestions", roomCode: id, playerId })
+      );
+    };
+
+    ws.onmessage = (event: MessageEvent) => {
+      const data = JSON.parse(event.data);
+
+      if (data.type === "questions" && data.roomCode === id) {
+        console.log(data.questions);
+      }
+    };
 
     // Naviguer vers l'écran de la room
-    navigate(`/room/${id}`);
+    // navigate(`/room/${id}`);
   };
 
   return (
@@ -80,32 +98,41 @@ export const WaitingRoomScreen: React.FC = () => {
         Code d'accès : <span className="text-[45px] ">{id}</span>
       </p>
 
-      <div className="w-full grid grid-cols-[1fr_1px_1fr] gap-10">
+      <div className="w-full grid grid-cols-[1fr_1px_1fr] gap-10 ">
         {/* Bloc pour la difficulté et le nombre de rounds */}
-        <div className="flex flex-col items-center justify-center gap-14 max-w-[80%] mx-auto w-full">
-          {/* Difficulté */}
+        <div className="flex flex-col items-center justify-center gap-14 max-w-[90%] ml-auto bg-white/70 p-5 rounded-lg w-full">
           <div className="flex justify-between items-center text-left w-full">
             <p className="text-[34px] w-full">Difficulté :</p>
-            <SelectInput
-              options={difficulties}
-              value={room?.difficulty}
-              onChange={(e) => handleUpdateRoom("difficulty", e.target.value)}
-            />
+            {isAdmin ? (
+              <SelectInput
+                options={difficulties}
+                value={room?.difficulty}
+                onChange={(e) => handleUpdateRoom("difficulty", e.target.value)}
+              />
+            ) : (
+              <p className="text-[24px]">
+                {difficulties.find((d) => d.value === room?.difficulty)?.label}
+              </p>
+            )}
           </div>
 
           {/* Nombre de rounds */}
           <div className="flex justify-between items-center text-center w-full">
             <p className="text-[34px]">Nombre de rounds :</p>
-            <TextInput
-              type="number"
-              value={room?.rounds}
-              onChange={(e) =>
-                handleUpdateRoom("rounds", parseInt(e.target.value))
-              }
-              min={1}
-              max={10}
-              className="w-[100px] h-[50px] text-[24px] text-center"
-            />
+            {isAdmin ? (
+              <TextInput
+                type="number"
+                value={room?.rounds}
+                onChange={(e) =>
+                  handleUpdateRoom("rounds", parseInt(e.target.value))
+                }
+                min={1}
+                max={10}
+                className="w-[100px] h-[50px] text-[24px] text-center"
+              />
+            ) : (
+              <p className="text-[24px]">{room?.rounds}</p>
+            )}
           </div>
 
           {/* Admin */}
@@ -120,7 +147,7 @@ export const WaitingRoomScreen: React.FC = () => {
         <div className="h-full w-[1px] bg-black" />
 
         {/* Liste des participants */}
-        <div className="flex flex-col items-center justify-center text-center gap-10">
+        <div className="flex flex-col items-center justify-center text-center gap-10 max-w-[90%] bg-white/70 p-5 rounded-lg">
           <p className="text-[34px]">
             Participants : {room?.players?.length}/8
           </p>
@@ -133,7 +160,11 @@ export const WaitingRoomScreen: React.FC = () => {
         </div>
       </div>
 
-      <OutlinedButton label="Lancer la partie" onClick={handleStartRoom} />
+      {isAdmin ? (
+        <OutlinedButton label="Lancer la partie" onClick={handleStartRoom} />
+      ) : (
+        <div />
+      )}
     </div>
   );
 };
